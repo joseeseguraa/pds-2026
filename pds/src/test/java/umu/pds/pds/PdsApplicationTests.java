3cc9769bd8d0b39dfc9808ff51170fd5f017cdd1
joseeseguraa <joseeseguraa1@gmail.com>
2026-03-30 16:18:49 +0200

    cambio de proyecto

---
+++ pds/src/test/java/umu/pds/pds/PdsApplicationTests.java
@@ -0,0 +1,13 @@
+package umu.pds.pds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
